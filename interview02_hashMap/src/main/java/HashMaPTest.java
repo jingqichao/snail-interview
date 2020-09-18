a67e3689cef13672684ba58e636ed7aab47255bb
荆启超 <1209446716@qq.com>
2020-09-18 22:24:53 +0800

    HashMap底层原理的学习

--- interview02_hashMap/src/main/java/HashMaPTest.java
+++ interview02_hashMap/src/main/java/HashMaPTest.java
@@ -4,10 +4,13 @@ import java.util.Map;
 public class HashMaPTest {
 
     public static void main(String[] args) {
+        //数组是连续的，链表不连续
+        //查询快，删除和插入慢；查询O(1),删除O(n)
         Map<String,String> map = new HashMap<String, String>();
         map.put("a","c");
         map.get("a");
         System.out.println(map.get("a"));
+        //Entry(key,value,hash,next)
     }
 
 }
